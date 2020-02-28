8da8566c4d5d43b3383e276a24ee5a2844e8cbae
kevacoin <info@kevacoin.org>
2020-02-27 23:58:09 -0800

    Added kevacoin address test.

--- test/address.spec.ts
+++ test/address.spec.ts
@@ -16,6 +16,16 @@ const NETWORKS = Object.assign(
       scriptHash: 0x32,
       wif: 0xb0,
     },
+    kevacoin: {
+      messagePrefix: '\x19Kevacoin Signed Message:\n',
+      bip32: {
+        public: 0x019da462,
+        private: 0x019d9cfe,
+      },
+      pubKeyHash: 0x2d,
+      scriptHash: 0x46,
+      wif: 0xb0,
+    },
   },
   require('../src/networks'),
 );
